fc27c2e5dc265f191ff74abe0fe406737dba87bc
Michiel Leunens <michiel.leunens@gmail.com>
2020-09-10 17:14:48 +0200

    Added App Icon and defined colors

--- Live Parking Ghent/View/Cells/ParkingCell.swift
+++ Live Parking Ghent/View/Cells/ParkingCell.swift
@@ -14,7 +14,7 @@ class ParkingCell: UITableViewCell {
     var safeArea: UILayoutGuide!
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
         setupView()
     }
 
@@ -26,6 +26,8 @@ class ParkingCell: UITableViewCell {
 
     func setupView() {
         safeArea = layoutMarginsGuide
+        
+        self.accessoryType = .disclosureIndicator
         setupNameLabel()
         setupAvailabilityLabel()
     }
--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## A demo app for In The Pocket.
 
 If using an emulator: note that location needs to be set in the emulator, and backgroundtasks won't work.
+
+## Design
+[Check on Figma](https://www.figma.com/file/bMcWeQPsCLTHwDA3MsmAzj/In-The-Pocket-Ghent-Parking?node-id=0%3A1)
+
